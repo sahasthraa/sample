bddab8e2b14b64cff66801bd149df7e94cf3ed5c
sahasthraa <sahasthrathummala@gmail.com>
2026-04-24 07:40:52 -0700

    loginVerifying

---
+++ ifElselogin.java
@@ -0,0 +1,13 @@
+public class ifElselogin {
+    public static void main(String args[]){
+      String username="sahasthra_thummala";
+      String password="sahasthra123";
+      if(username.equals("sahasthra_thummala") && password.equals("sahasthra@123")){
+        System.out.println("login successful,wecome!");
+
+      }else{
+        System.out.println("login failed,try again");
+      }
+    }}
+    
+
