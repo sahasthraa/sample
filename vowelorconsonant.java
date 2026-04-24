7f80e48e14043ee5bcea6e8dcb9c501dd34999a9
sahasthraa <sahasthrathummala@gmail.com>
2026-04-24 07:28:18 -0700

    switching vowels

---
+++ vowelorconsonant.java
@@ -0,0 +1,26 @@
+public class vowelorconsonant {
+    public static void main(String args[]){
+        char ch='e';
+        switch(ch){
+        case 'a':
+            System.out.println("a is a vowel");
+            break;
+            case 'e':
+                System.out.println("e is a vowel");
+                break;
+                case 'i':
+                    System.out.println("i is a vowel");
+                    break;
+                    case 'o':
+                        System.out.println("o is a vowel");
+                    break;
+                    case 'u':
+                        System.out.println("u is a vowel");
+                    break;
+                    default:
+                        System.out.println("it is a consonant");
+                        break;
+
+    }
+} 
+}
