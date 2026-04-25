1b7919a3c9f43b375b0cc75247d7fb802d3a198e
sahasthraa <sahasthrathummala@gmail.com>
2026-04-25 10:02:27 -0700

    false condtn

---
+++ doWhileFF.java
@@ -0,0 +1,10 @@
+f class doWhileFF{
+    public static void main(String args[]){
+    int i=1;                          
+        do{
+            System.out.println(i);
+            i++;
+           } while(i<1);
+        
+    }
+}false
