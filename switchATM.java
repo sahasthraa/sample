d721c60db80efe8e86840ea1fc87feb335d46186
sahasthraa <sahasthrathummala@gmail.com>
2026-04-25 06:49:25 -0700

    actions added

---
+++ switchATM.java
@@ -0,0 +1,25 @@
+public class switchATM{
+    public static void main(String arg[]){
+        int pin=2006;
+        if(pin==2006){
+            switch (action)=("withdrawl");
+            case "deposit":
+            System.out.println("deposit successful");
+            break;
+            case "withdrawl":
+            System.out.println("withdrawl successful");
+            break;
+            case "balance enquiry":
+            System.out.println("available balance");
+            break;
+            default:
+                System.out.println("go out");
+                break;      
+             
+        }
+        else{
+            System.out.println("invalid pin,please try again");
+
+        }
+    }
+}
