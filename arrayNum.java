29877b195da6cdc6267471e95cdb246a4627dfd2
sahasthraa <sahasthrathummala@gmail.com>
2026-05-06 09:43:22 -0700

    giving values to array

---
+++ arrayNum.java
@@ -0,0 +1,13 @@
+
+
+public class arrayNum {
+    public static void main(String args[]){
+int[] numbers={13,65,43,90,23};
+        
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[4]);
+    }
+}
