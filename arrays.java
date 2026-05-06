37ff7823e856126708fa486a9e570cf3d16462cd
sahasthraa <sahasthrathummala@gmail.com>
2026-05-06 09:33:51 -0700

    numprinting

---
+++ arrays.java
@@ -0,0 +1,16 @@
+public class arrays{
+    public static void main(String args[]){
+        int[] numbers=new int[5];
+        numbers[0]=13;
+        numbers[1]=65;
+        numbers[2]=43;
+        numbers[3]=90;
+        numbers[4]=87;
+        
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[4]);
+    }
+}
