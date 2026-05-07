7cfdf9163625d3b3382654990f9bc335c3d75231
sahasthraa <sahasthrathummala@gmail.com>
2026-05-07 07:54:10 -0700

    foreach2 Array

--- ArrTraversal.java
+++ ArrTraversal.java
@@ -1,6 +1,14 @@
 public class ArrTraversal{
     public static void main(String args[]){
-        int[] numbers=new int[]{12,23,54,67,84,62,19,50};
+        int[] numbers=new int[6];
+        numbers[0]=23;
+        numbers[1]=54;
+        numbers[2]=98;
+        numbers[3]=12;
+        numbers[4]=67;
+        numbers[5]=34;
+        
+
         for( int num: numbers){
             System.out.println(num);
         }
