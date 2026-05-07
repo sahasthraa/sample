70f501469f2734dc76029cba9509ea2689879408
sahasthraa <sahasthrathummala@gmail.com>
2026-05-07 07:37:30 -0700

    arraytraversal using for loop

---
+++ arrayTraversal.java
@@ -0,0 +1,9 @@
+public class arrayTraversal{
+    public static void main(String args[]){
+        int[] numbers= new int[]{22,54,67,92,13,41};
+
+        for(int i=0;i<numbers.length;i++){
+            System.out.println(numbers[i]);
+        }
+    }
+}
