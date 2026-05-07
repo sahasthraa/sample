1e280f561cacf482a6e13ceeff0bbfb86f7c73b7
sahasthraa <sahasthrathummala@gmail.com>
2026-05-07 07:50:23 -0700

    ArrTraversal using for each loop

---
+++ ArrTraversal.java
@@ -0,0 +1,8 @@
+public class ArrTraversal{
+    public static void main(String args[]){
+        int[] numbers=new int[]{12,23,54,67,84,62,19,50};
+        for( int num: numbers){
+            System.out.println(num);
+        }
+    }
+}
