0435b9adc0ac020b58817da1bdd7b97872752d91
sahasthraa <sahasthrathummala@gmail.com>
2026-05-08 07:18:29 -0700

    sum using for

--- arraySum.java
+++ arraySum.java
@@ -1,5 +1,5 @@
 public class arraySum{
-    public static void min(String args[]){
+    public static void main(String args[]){
         int [] numbers={43,56,90,62,39,12,17,83};
         int sum=0;
         for(int i=0;i<numbers.length;i++){
